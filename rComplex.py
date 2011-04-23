434d5c337eb3549f7293c3776dbd355b19f167d9
Manuel Bärenz <manuel@enigmage.de>
2011-04-23 10:14:59 +0200

    Complex abs property und rechner / und -

--- rComplex.py
+++ rComplex.py
@@ -4,18 +4,26 @@
 import math
 
 class Complex(object):
-  def __init__(self, a, b=0):
-    if isinstance(a,str):
-      self.real = float(a[:a.find(',')])
-      self.imag = float(a[a.find(',')+1:])
-    else:
-      self.real = a
-      self.imag = b
-  def abs(self):
-    return math.sqrt(self.real * self.real + self.imag * self.imag)
-  def __str__(self):
-    return str(self.real) + ',' + str(self.imag)
-  def __add__(self, summand):
-    return Complex(self.real + summand.real, self.imag + summand.imag)
-  def __mul__(self, summand):
-    return Complex(self.real*summand.real-self.imag*summand.imag, self.real*summand.imag+self.imag*summand.real)
+	def __init__(self, a, b=0):
+		if isinstance(a,str):
+			self.real = float(a[:a.find(',')])
+			self.imag = float(a[a.find(',')+1:])
+		else:
+			self.real = a
+			self.imag = b
+	def get_abs(self):
+		return math.sqrt(self.real * self.real + self.imag * self.imag)
+	def set_abs(self, abs_):
+		streckfaktor = float(abs_)/self.get_abs()
+		self.real = self.real * streckfaktor
+		self.imag = self.imag * streckfaktor
+	abs = property(get_abs, set_abs)
+	def __str__(self):
+		return str(self.real) + ',' + str(self.imag)
+	def __add__(self, summand):
+		return Complex(self.real + summand.real, self.imag + summand.imag)
+	def __mul__(self, faktor):
+		if isinstance(faktor, Complex):
+			return Complex(self.real*faktor.real-self.imag*faktor.imag, self.real*faktor.imag+self.imag*faktor.real)
+		else:
+			return Complex(self.real*faktor, self.imag*faktor)
--- rSpielwiese.py
+++ rSpielwiese.py
@@ -5,27 +5,36 @@
 #~ 
 #~ zahl1 = rComplex.Complex('3,2')
 #~ zahl2 = rComplex.Complex(1)
+#~ 
+#~ print 'Der Betrag ist', zahl1.abs, '.'
+#~ 
+#~ print 'Der Realteil ist', zahl1.real, '.'
+#~ print 'Der Imaginärteil ist', zahl1.imag, '.'
+#~ 
+#~ neuer_betrag = 9
+#~ print "Setze auf Betrag", neuer_betrag
+#~ zahl1.abs = neuer_betrag
+#~ 
+#~ print 'Der Betrag ist', zahl1.get_abs(), '.'
+#~ print 'Der Realteil ist', zahl1.real, '.'
+#~ print 'Der Imaginärteil ist', zahl1.imag, '.'
 
-#print 'Der Betrag ist', zahl2.abs(), '.'
-
-#print 'Der Realteil ist', zahl2.real, '.'
-#print 'Der Imaginärteil ist', zahl2.imag, '.'
 
 #~ print zahl1 + zahl2
 #~ print zahl1*zahl2
 
 #zahl von knuddel
 
-import knuddel
-
-bla = knuddel.Knuddel("Raffaela")
-
-blub = knuddel.Knuddel("Manuel")
-
-bla.begruessen(blub)
-bla.knuddeln(blub)
-
-baby = bla + blub
-
-baby.benennen("Lilly") 
-baby.vorstellung()
+#~ import knuddel
+#~ 
+#~ bla = knuddel.Knuddel("Raffaela")
+#~ 
+#~ blub = knuddel.Knuddel("Manuel")
+#~ 
+#~ bla.begruessen(blub)
+#~ bla.knuddeln(blub)
+#~ 
+#~ baby = bla + blub
+#~ 
+#~ baby.benennen("Lilly") 
+#~ baby.vorstellung()
